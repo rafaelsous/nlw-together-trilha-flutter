690b24dd80af5ea0a036674549aeed4652f4209e
Rafael Lima de Sousa <rafaelsousa.souza@gmail.com>
2021-06-25 15:07:58 -0300

    Added insert boleto page.

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import '/modules/insert_boleto/insert_boleto_page.dart';
 import '/modules/barcode_scanner/barcode_scanner_page.dart';
 import '/modules/home/home_page.dart';
 import '/modules/splash/splash_page.dart';
@@ -31,6 +32,7 @@ class AppWidget extends StatelessWidget {
         '/home': (context) => HomePage(),
         '/login': (context) => LoginPage(),
         '/barcode_scanner': (context) => BarcodeScannerPage(),
+        '/insert_boleto': (context) => InsertBoletoPage(),
       },
     );
   }
